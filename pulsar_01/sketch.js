d6f7f4d4f4e5c961abb018fa8325625d4e956bba
chrischne <chrischne@gmail.com>
2021-04-28 15:05:40 +0200

    pulsar 01 pulsar 02

--- pulsar_01/sketch.js
+++ pulsar_01/sketch.js
@@ -1,6 +1,7 @@
 let data = [];
 
 let xScale = d3.scaleLinear();
+let yScale = d3.scaleLinear();
 
 let ready = false;
 
@@ -12,12 +13,19 @@ function setup() {
       return +v;
     });
 
-    return {
-      values: values,
-    };
+    // return {
+    //   values: values,
+    // };
+    return values;
   }).then(function (csv) {
     // console.log(csv);
     data = csv;
+    allarrays = [];
+    allarrays = allarrays.concat(...data);
+    console.log("allarrays", allarrays);
+    let extent = d3.extent(allarrays);
+    console.log("extent", extent);
+    yScale.domain(extent).range([100, 0]);
     xScale.domain([0, 255]).range([0, width]);
     ready = true;
     redraw();
@@ -33,14 +41,14 @@ function draw() {
   }
   background(200);
 
-  var currentData = data[0].values;
+  var currentData = data[0];
   console.log(currentData);
 
   beginShape();
   for (let i = 0; i < currentData.length; i++) {
     let x = xScale(i);
-    console.log("x from", i, "=", x);
-    let y = currentData[i];
+    // console.log("x from", i, "=", x);
+    let y = yScale(currentData[i]);
     vertex(x, y);
   }
   endShape();
---
+++ pulsar_02/sketch.js
@@ -0,0 +1,64 @@
+let data = [];
+
+let xScale = d3.scaleLinear();
+let yScale = d3.scaleLinear();
+
+let ready = false;
+
+function setup() {
+  createCanvas(500, 700);
+  d3.csv("pulsar2.csv", function (d) {
+    let values = Object.values(d);
+    values = values.map((v) => {
+      return +v;
+    });
+
+    // return {
+    //   values: values,
+    // };
+    return values;
+  }).then(function (csv) {
+    // console.log(csv);
+    data = csv;
+    allarrays = [];
+    allarrays = allarrays.concat(...data);
+    console.log("allarrays", allarrays);
+    let extent = d3.extent(allarrays);
+    console.log("extent", extent);
+    yScale.domain(extent).range([0, -150]);
+    xScale.domain([0, 255]).range([0, width]);
+    ready = true;
+    redraw();
+  });
+
+  noLoop();
+}
+
+function draw() {
+  if (!ready) {
+    background(255, 0, 0);
+    return;
+  }
+  background(200);
+
+  console.log(currentData);
+
+  for (let i = 0; i < data.length; i++) {
+    var currentData = data[i];
+    push();
+    translate(0, i * 20);
+    lineChart(currentData);
+    pop();
+  }
+}
+
+function lineChart(chartData) {
+  beginShape();
+  for (let i = 0; i < chartData.length; i++) {
+    let x = xScale(i);
+    // console.log("x from", i, "=", x);
+    let y = yScale(chartData[i]);
+    vertex(x, y);
+  }
+  endShape();
+}
