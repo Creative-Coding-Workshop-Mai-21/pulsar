572f41606f03a41e035c8a41f36aff25ab240306
chrischne <chrischne@gmail.com>
2021-04-29 10:37:11 +0200

    add base code

---
+++ pulsar_01_base_code/sketch.js
@@ -0,0 +1,22 @@
+let data = [];
+
+function setup() {
+  createCanvas(600, 700);
+  data = await d3.csv("pulsar.csv", parseRow);
+
+  // we only need to draw the scene once
+  noLoop();
+}
+
+function draw() {
+  background(200);
+
+}
+
+function parseRow(d) {
+  let valueArray = Object.values(d);
+  valueArray = valueArray.map((v) => {
+    return +v;
+  });
+  return valueArray;
+}
